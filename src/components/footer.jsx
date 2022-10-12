068c302cf2708320e1fc94467c4799a975710c11
Burger1w <104843190+Burger1w@users.noreply.github.com>
2022-10-12 20:56:59 +0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,7 +10,7 @@ export default function Footer(){
             <hr className="divider w-full"></hr>
             <div className="block space-y-4 sm:space-y-0 sm:flex sm:justify-between sm:items-center">
                 <h1 className="font-sans text-gray-900 dark:text-white text-base">Copyright © 2022</h1>
-                <h1 className="font-sans text-gray-900 dark:text-white text-base flex gap-2 items-center">Made with <SiReact color="AQUA"/> React by <a href={`https://github.com/${UserData.github}`}>Just Nyde</a></h1>
+                <h1 className="font-sans text-gray-900 dark:text-white text-base flex gap-2 items-center">Made with <SiReact color="AQUA"/> React by <a href={`https://github.com/${UserData.github}`}>kadybot</a></h1>
             </div>
         </div>
         <br/>
